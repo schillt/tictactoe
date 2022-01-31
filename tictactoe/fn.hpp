0e1be7d28cea99cbb7fe25a84ba7dad479fb8cef
Tyler Schill <98628991+schillt@users.noreply.github.com>
2022-01-30 22:55:43 -0700

    Update fn.hpp

--- tictactoe/fn.hpp
+++ tictactoe/fn.hpp
@@ -1,5 +1,15 @@
 using namespace std;
 
+/****************************************************************
+* I realized vectors are not like arrays and when passed to 
+* a function only a copy of the vector is passed, whereas an
+* array would be passed with all of its data (I think). As such,
+* the board vector needs to be passed by reference so changes
+* made by players on the turn are reflected. I am not sure if
+* the two bools require it, but I think so, though I think they
+* could both be const if needed. I made them references anyway
+****************************************************************/
+
 void displayBoard(vector<string> board);
 void instructions();
 void p1Turn(vector<string> &board, int turn);
